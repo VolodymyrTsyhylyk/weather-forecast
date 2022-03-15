11bb5813fca1e5be10ca1e851c72350c067e342d
Volodymyr Tsyhylyk <82731850+VolodymyrTsyhylyk@users.noreply.github.com>
2022-03-16 01:58:02 +0200

    working version

---
+++ src/components/info.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Info = () => (
+            <div>
+                <h2>Прогноз погоди</h2>
+                <p>Дізнайся погоду у вашому місті!</p>
+            </div>
+);
+
+
+export default Info;
